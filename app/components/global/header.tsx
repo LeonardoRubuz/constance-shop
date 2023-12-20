cb10af5fff3fd63610197003e38445d85f919f3e
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-20 22:16:28 +0100

    hero modifications started

--- app/components/global/header.tsx
+++ app/components/global/header.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
 import Logo from "./logo";
-import { ShoppingCartIcon, UserIcon } from "@heroicons/react/24/outline";
+import { ShoppingCartIcon, UserIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 
 export default function Header() {
     return(
-        <header className="flex flex-row justify-between p-5 sticky top-0 font-medium">
+        <header className="flex flex-row justify-evenly gap-[8rem] p-5 sticky top-0 font-medium bg-blanc">
             <Logo />
-            <nav className="flex justify-between gap-[5rem]">
-                <div>Discovery</div>
+            <nav className="flex justify-between gap-[5rem] font-semibold">
+                <div className="flex ">
+                    <span>Discovery </span>
+                    <ChevronDownIcon className="h-6 w-4 ml-2 text-noir" />
+                </div>
                 <div>About</div>
                 <div>Contact us</div>
             </nav>
--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -2,13 +2,13 @@ import CallToAction from "../global/callToAction";
 
 function Hero() {
     return ( 
-        <section>
-            <div>
+        <section className="flex align-left h-[30rem] bg-hero-bg bg-no-repeat bg-cover">
+            <div className="border-2 ml-[5rem] my-7 w-[20%]">
                 <div>
-                    <h2>The nature candle</h2>
-                    <p>All handmade with natural soy wax, Candleaf is a companion for all your pleasure</p>
-               </div>
-               <CallToAction text="Discovery our collection" hasIcon={false} />
+                    <h1>The nature candle</h1>
+                    <p>All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments</p>
+                </div>
+                <CallToAction text="Discover our collection" hasIcon={false} />
             </div>
         </section>
      );
